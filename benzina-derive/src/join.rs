b6f5c0a9845ea5877fffd57974b24cb174026399
Paolo Barbolini <paolo.barbolini@m4ss.net>
2025-07-23 10:27:07 +0200

    wip

--- benzina-derive/src/join.rs
+++ benzina-derive/src/join.rs
@@ -1,6 +1,6 @@
 use std::collections::BTreeMap;
 
-use proc_macro2::TokenStream;
+use proc_macro2::{Span, TokenStream};
 use quote::{ToTokens, quote};
 use syn::{
     Ident, Index, LitInt, Token, Type, TypePath, braced,
@@ -111,16 +111,16 @@ impl NestedOrNot {
         }
     }
 
-    fn or_insert(&self, can_be_zero: bool) -> TokenStream {
+    fn or_insert(&self, tuple_index_overwrites: &BTreeMap<usize, TokenStream>) -> TokenStream {
         match self {
             Self::Nested(nested) => {
                 let values = nested
                     .iter()
-                    .flat_map(|item| item.or_insert())
+                    .flat_map(|item| item.or_insert(tuple_index_overwrites))
                     .collect::<TokenStream>();
                 quote! { HashMap::<_, _>::new(), }
             }
-            Self::Not(not) => not.or_insert(can_be_zero),
+            Self::Not(not) => not.or_insert(tuple_index_overwrites),
         }
     }
 }
@@ -164,16 +164,16 @@ impl Transformation {
             .unwrap();
         let one_tuple_index = Index::from(one.tuple_index);
 
-        let (wrapper, unwrapper) = if matches!(self.quantity, Quantity::AtLeastZero) {
-            (
-                quote! { if row.#one_tuple_index.is_some() },
-                quote! { .unwrap() },
-            )
+        let mut tuple_index_overwrites = BTreeMap::new();
+        let wrapper = if matches!(self.quantity, Quantity::AtLeastZero) {
+            let name = Ident::new(&format!("unwrapped{}", one.tuple_index), Span::call_site());
+            tuple_index_overwrites.insert(one.tuple_index, quote! { #name });
+            quote! { if let Some(#name) = row.#one_tuple_index }
         } else {
-            (quote! {}, quote! {})
+            quote! {}
         };
 
-        let or_insert = self.or_insert();
+        let or_insert = self.or_insert(&tuple_index_overwrites);
         let entries_mapper = self
             .entries
             .iter()
@@ -181,18 +181,23 @@ impl Transformation {
             .map(|(i, (_name, entry))| entry.row_handlers(i))
             .collect::<TokenStream>();
 
+        let one_name = if let Some(overwrite) = tuple_index_overwrites.get(&one.tuple_index) {
+            overwrite.clone()
+        } else {
+            quote! { row.#one_tuple_index }
+        };
         quote! {
             #wrapper {
-                let mut root = #root_index.entry(identifiable_id(&row.#one_tuple_index #unwrapper).clone()).or_insert((#or_insert));
+                let mut root = #root_index.entry(identifiable_id(&#one_name).clone()).or_insert((#or_insert));
                 #entries_mapper
             }
         }
     }
 
-    fn or_insert(&self) -> TokenStream {
+    fn or_insert(&self, tuple_index_overwrites: &BTreeMap<usize, TokenStream>) -> TokenStream {
         self.entries
             .iter()
-            .map(|(_name, entry)| entry.or_insert(matches!(self.quantity, Quantity::AtLeastZero)))
+            .map(|(_name, entry)| entry.or_insert(tuple_index_overwrites))
             .collect()
     }
 }
@@ -233,30 +238,41 @@ impl NoTransformation {
         let root_index = Index::from(root_index);
         match self.quantity {
             Quantity::MaybeOne => quote! {
-                if let Some(item) = row.#tuple_index {
-                    root.#root_index = Some(item);
+                {
+                    if let Some(item) = row.#tuple_index {
+                        root.#root_index = Some(item);
+                    }
                 }
             },
             Quantity::One => quote! {},
             Quantity::AtLeastZero => quote! {
-                if let Some(item) = row.#tuple_index {
-                    root.#root_index.insert(identifiable_id(&item).clone(), item);
+                {
+                    if let Some(item) = row.#tuple_index {
+                        root.#root_index.entry(identifiable_id(&item).clone()).or_insert(item);
+                    }
                 }
             },
             Quantity::AtLeastOne => quote! {
-                let item = row.#tuple_index;
-                root.#root_index.insert(identifiable_id(&item).clone(), item);
+                {
+                    let item = row.#tuple_index;
+                    root.#root_index.entry(identifiable_id(&item).clone()).or_insert(item);
+                }
             },
         }
     }
 
-    fn or_insert(&self, can_be_zero: bool) -> TokenStream {
-        let tuple_index = Index::from(self.tuple_index);
-        match (self.quantity, can_be_zero) {
-            (Quantity::MaybeOne, _) => quote! { None, },
-            (Quantity::One, false) => quote! { row.#tuple_index, },
-            (Quantity::One, true) => quote! { row.#tuple_index.unwrap(), },
-            (Quantity::AtLeastZero | Quantity::AtLeastOne, _) => {
+    fn or_insert(&self, tuple_index_overwrites: &BTreeMap<usize, TokenStream>) -> TokenStream {
+        match self.quantity {
+            Quantity::MaybeOne => quote! { None, },
+            Quantity::One => {
+                if let Some(overwrite) = tuple_index_overwrites.get(&self.tuple_index) {
+                    quote! { #overwrite, }
+                } else {
+                    let tuple_index = Index::from(self.tuple_index);
+                    quote! { row.#tuple_index, }
+                }
+            }
+            Quantity::AtLeastZero | Quantity::AtLeastOne => {
                 quote! { HashMap::<_, _>::new(), }
             }
         }

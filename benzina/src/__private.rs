6bf739aa03e62551e46e739c509e33aeb1586248
Paolo Barbolini <paolo.barbolini@m4ss.net>
2025-08-25 15:16:11 +0200

    fix(join!): joining with tables having composite primary keys

--- benzina/src/__private.rs
+++ benzina/src/__private.rs
@@ -22,3 +22,53 @@ pub type IndexMap<K, V> = indexmap::IndexMap<K, V, Hasher>;
 pub fn new_indexmap<K, V>() -> IndexMap<K, V> {
     IndexMap::with_hasher(Hasher::default())
 }
+
+pub mod deep_clone {
+    pub trait DeepClone {
+        type Output;
+
+        fn deep_clone(&self) -> Self::Output;
+    }
+
+    impl<T: Clone> DeepClone for &T {
+        type Output = T;
+
+        fn deep_clone(&self) -> Self::Output {
+            (*self).clone()
+        }
+    }
+
+    #[rustfmt::skip]
+    mod impls {
+        use super::DeepClone;
+
+        macro_rules! impl_deep_clone_for_tuples {
+            ($(($T:ident, $idx:tt)),+) => {
+                impl<$($T: DeepClone),+> DeepClone for ($($T),+,) {
+                    type Output = ($(<$T as DeepClone>::Output),+,);
+
+                    fn deep_clone(&self) -> Self::Output {
+                        ($((&self).$idx.deep_clone()),+,)
+                    }
+                }
+            };
+        }
+
+        impl_deep_clone_for_tuples!((T1, 0));
+        impl_deep_clone_for_tuples!((T1, 0), (T2, 1));
+        impl_deep_clone_for_tuples!((T1, 0), (T2, 1), (T3, 2));
+        impl_deep_clone_for_tuples!((T1, 0), (T2, 1), (T3, 2), (T4, 3));
+        impl_deep_clone_for_tuples!((T1, 0), (T2, 1), (T3, 2), (T4, 3), (T5, 4));
+        impl_deep_clone_for_tuples!((T1, 0), (T2, 1), (T3, 2), (T4, 3), (T5, 4), (T6, 5));
+        impl_deep_clone_for_tuples!((T1, 0), (T2, 1), (T3, 2), (T4, 3), (T5, 4), (T6, 5), (T7, 6));
+        impl_deep_clone_for_tuples!((T1, 0), (T2, 1), (T3, 2), (T4, 3), (T5, 4), (T6, 5), (T7, 6), (T8, 7));
+        impl_deep_clone_for_tuples!((T1, 0), (T2, 1), (T3, 2), (T4, 3), (T5, 4), (T6, 5), (T7, 6), (T8, 7), (T9, 8));
+        impl_deep_clone_for_tuples!((T1, 0), (T2, 1), (T3, 2), (T4, 3), (T5, 4), (T6, 5), (T7, 6), (T8, 7), (T9, 8), (T10, 9));
+        impl_deep_clone_for_tuples!((T1, 0), (T2, 1), (T3, 2), (T4, 3), (T5, 4), (T6, 5), (T7, 6), (T8, 7), (T9, 8), (T10, 9), (T11, 10));
+        impl_deep_clone_for_tuples!((T1, 0), (T2, 1), (T3, 2), (T4, 3), (T5, 4), (T6, 5), (T7, 6), (T8, 7), (T9, 8), (T10, 9), (T11, 10), (T12, 11));
+        impl_deep_clone_for_tuples!((T1, 0), (T2, 1), (T3, 2), (T4, 3), (T5, 4), (T6, 5), (T7, 6), (T8, 7), (T9, 8), (T10, 9), (T11, 10), (T12, 11), (T13, 12));
+        impl_deep_clone_for_tuples!((T1, 0), (T2, 1), (T3, 2), (T4, 3), (T5, 4), (T6, 5), (T7, 6), (T8, 7), (T9, 8), (T10, 9), (T11, 10), (T12, 11), (T13, 12), (T14, 13));
+        impl_deep_clone_for_tuples!((T1, 0), (T2, 1), (T3, 2), (T4, 3), (T5, 4), (T6, 5), (T7, 6), (T8, 7), (T9, 8), (T10, 9), (T11, 10), (T12, 11), (T13, 12), (T14, 13), (T15, 14));
+        impl_deep_clone_for_tuples!((T1, 0), (T2, 1), (T3, 2), (T4, 3), (T5, 4), (T6, 5), (T7, 6), (T8, 7), (T9, 8), (T10, 9), (T11, 10), (T12, 11), (T13, 12), (T14, 13), (T15, 14), (T16, 15));
+    }
+}
